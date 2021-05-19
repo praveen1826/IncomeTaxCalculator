5ebd9cea283b8d92f6b8fa440788ec8ba326f8e0
praveen1826 <thiruttanipraveen@gmail.com>
2021-05-19 13:17:57 +0530

    final code update

--- src/com/company/SuperSeniorCitizen.java
+++ src/com/company/SuperSeniorCitizen.java
@@ -4,6 +4,11 @@ public class SuperSeniorCitizen implements AgeStrategy{
 
     @Override
     public double taxExemption(double taxxxx) {
-        return 0;
+        if(taxxxx <= 500000){
+            System.out.println("Exempt From Tax Until 500000");
+            double FinalTax= 0;
+            System.out.println("Your Final Tax Is:    "+FinalTax);
+            return FinalTax;
+        }return taxxxx;
     }
 }
